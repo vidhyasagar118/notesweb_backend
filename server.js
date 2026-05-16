1d78bfc971620d27da1a522c7f7ee57751f9e4e5
vidhyasagar118 <YOUR_GITHUB_EMAIL>
2026-05-16 14:32:33 +0530

    server startic use line remove

--- server.js
+++ server.js
@@ -15,39 +15,32 @@ app.use(cors());
 
 app.use(express.json());
 
-/* IMPORTANT FIX */
 app.use(
-    "/uploads",
-    express.static(
-        path.join(__dirname, "uploads"),
-        {
-            setHeaders: (res) => {
-                res.set("Access-Control-Allow-Origin", "*");
-            }
-        }
-    )
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"))
 );
+app.use("/uploads", express.static("uploads"));
 
 app.use("/api/auth", authRoutes);
 
 app.use("/api/files", fileRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Backend Running 🚀");
+  res.send("Backend Running 🚀");
 });
 
 mongoose.connect(process.env.MONGO_URI)
 
 .then(() => {
 
-    console.log("MongoDB Connected");
+  console.log("MongoDB Connected");
 
-    app.listen(process.env.PORT || 5000, () => {
-        console.log("Server running");
-    });
+  app.listen(process.env.PORT || 5000, () => {
+    console.log("Server running");
+  });
 
 })
 
 .catch((err) => {
-    console.log(err);
+  console.log(err);
 });
