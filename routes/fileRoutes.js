399cfa6f559e3c8e3091f29c077c54a62b2d3517
vidhyasagar118 <YOUR_GITHUB_EMAIL>
2026-05-17 09:15:10 +0530

    fix temp folder error

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -31,7 +31,7 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 50 * 1024 * 1024
+        fileSize: 10 * 1024 * 1024   // ✅ 10MB
     }
 });
 
