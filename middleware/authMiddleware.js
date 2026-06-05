bf043a3e1628416b3fa931e4309504572fee0fd9
vidhyasagar118 <YOUR_GITHUB_EMAIL>
2026-06-05 17:16:28 +0530

    upload

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,26 +2,34 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
 
-    const authHeader = req.header("Authorization");
 
-    if (!authHeader) {
-        return res.status(401).json({ message: "No token" });
-    }
+const authHeader = req.header("Authorization");
 
-    try {
+// ✅ Proper check
+if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token" });
+}
 
-        if (!authHeader.startsWith("Bearer ")) {
-            return res.status(401).json({ message: "Invalid format" });
-        }
+try {
 
-        const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[1];
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET
+    );
 
-        req.user = decoded;
-        next();
+    req.user = decoded;
+    next();
 
-    } catch (err) {
-        res.status(401).json({ message: "Invalid token" });
-    }
+} catch (err) {
+
+    console.log("JWT ERROR:", err);
+
+    return res.status(401).json({
+        message: "Invalid token"
+    });
+}
+
+
 };
